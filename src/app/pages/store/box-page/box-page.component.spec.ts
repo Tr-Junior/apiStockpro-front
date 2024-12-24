c6eea0fb128dafecc257fc20b503ee75b9b016c3
Trjunior <tarcisio.junior1406@gmail.com>
2024-12-24 11:24:00 -0300

    create box-page.component.css and box-page.component.spec.ts

---
+++ src/app/pages/store/box-page/box-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoxPageComponent } from './box-page.component';
+
+describe('BoxPageComponent', () => {
+  let component: BoxPageComponent;
+  let fixture: ComponentFixture<BoxPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BoxPageComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(BoxPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
