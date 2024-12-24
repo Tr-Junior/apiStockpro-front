62e07bdfa15194e82d3923ad690300c9310cf845
Trjunior <tarcisio.junior1406@gmail.com>
2024-12-24 11:25:03 -0300

    create supplier-page.component.css, supplier-page.component.html, supplier-page.component.spec.ts and supplier-page.component.ts

---
+++ src/app/pages/store/supplier-page/supplier-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './supplier-page.component.html',
+  styleUrl: './supplier-page.component.css'
+})
+export class SupplierPageComponent {
+
+}
