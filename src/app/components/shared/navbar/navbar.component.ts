dc5f14c2c5edc7171cd5d14001fc96f90e375e54
Trjunior <tarcisio.junior1406@gmail.com>
2025-01-24 19:02:35 -0300

    update navbar.component.ts

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,98 +1,90 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MenuItem, MessageService } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
 import { CommonModule } from '@angular/common';
 import { Router, NavigationEnd } from '@angular/router';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
-import { filter } from 'rxjs/operators';
 import { Security } from '../../../../utils/Security.util';
 import { User } from '../../../../models/user.model';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { MenuModule } from 'primeng/menu';
+import { DataService } from '../../../../services/data.service';
+import { ImportsService } from '../../../../services/imports.service';
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.css'],
     standalone: true,
-    imports: [MenubarModule, CommonModule, AvatarModule, ButtonModule, OverlayPanelModule, MenuModule],
+    imports: [MenubarModule, CommonModule, AvatarModule, ButtonModule, OverlayPanelModule, MenuModule, ImportsService.imports],
+      providers: [ImportsService.providers, DataService, MessageService],
 })
 export class NavbarComponent implements OnInit {
-    items: MenuItem[] = [];
-    activeItem: MenuItem | undefined;
-    public user!: User;
-    userItems: MenuItem[] = [];
+  items: MenuItem[] = [];
+  activeItem: MenuItem | undefined;
+  public user!: User;
+  userItems: MenuItem[] = [];
+  logoUrl: string = ''; // Variável para armazenar a URL da logo
 
+  constructor(private router: Router, private service: DataService) {}
 
-    constructor(private router: Router) {}
+  ngOnInit() {
+      this.user = Security.getUser();
+      this.items = [
+          { label: 'Produtos', icon: 'pi pi-list', routerLink: '/store' },
+          { label: 'Frente de caixa', icon: 'pi pi-cart-plus', routerLink: '/sale' },
+          { label: 'Orçamentos', icon: 'pi pi-clipboard', routerLink: '/budgets' },
+          { label: 'Pedidos', icon: 'pi pi-list-check', routerLink: '/productsToBuy' },
+          {
+              label: 'Faturamento',
+              icon: 'pi pi-external-link',
+              items: [
+                  { label: 'Vendas', icon: 'pi pi-dollar', routerLink: '/sales' },
+                  { label: 'Faturamento mensal', icon: 'pi pi-chart-bar', routerLink: '/features/details' },
+                  { label: 'Saídas', icon: 'pi pi-chart-line', routerLink: '/features/exits' }
+              ]
+          }
+      ];
 
-    ngOnInit() {
-    this.user = Security.getUser();
-        this.items = [
-            {
-                label: 'Produtos',
-                icon: 'pi pi-list',
-                routerLink: '/store',
-            },
-            {
-                label: 'Frente de caixa',
-                icon: 'pi pi-cart-plus',
-                routerLink: '/sale',
-            },
-            {
-                label: 'Orçamentos',
-                icon: 'pi pi-clipboard',
-                routerLink: '/budgets',
-            },
-            {
-              label: 'Pedidos',
-              icon: 'pi pi-list-check',
-              routerLink: '/productsToBuy',
-            },
-            {
-                label: 'Faturamento',
-                icon: 'pi pi-external-link',
-                  items:[
-                    {
-                        label: 'Vendas',
-                        icon: 'pi pi-dollar',
-                        routerLink: '/sales',
-                    },
-                    {
-                      label: 'Faturamento mensal',
-                      icon: 'pi pi-chart-bar',
-                      routerLink: '/features/details',
-                  },
-                  {
-                    label: 'saidas',
-                    icon: 'pi pi-chart-line',
-                    routerLink: '/features/exits',
-                }
-                  ]
-
-            },
-        ];
-        this.userItems = [
+      this.userItems = [
           { label: 'Cadastro de usuário', icon: 'pi pi-user-plus', routerLink: '/account/new-user' },
-          { label: 'Alterar senha', icon: 'pi pi-key', routerLink: '/account/passwordChange' }
-        ];
-        // Atualiza o item ativo com base na rota atual ao inicializar
-        this.updateActiveItem(this.router.url);
+          { label: 'Alterar senha', icon: 'pi pi-key', routerLink: '/account/passwordChange' },
+          { label: 'Informações da empresa', icon: 'pi pi-key', routerLink: '/company' }
+      ];
 
-        // Escuta mudanças de rota para atualizar o item ativo dinamicamente
-        this.router.events
-            .pipe(filter((event) => event instanceof NavigationEnd))
-            .subscribe((event: any) => {
-                this.updateActiveItem(event.urlAfterRedirects);
-            });
-    }
+      this.updateActiveItem(this.router.url);
 
-    updateActiveItem(url: string) {
-        this.activeItem = this.items.find((item) => item.routerLink === url);
-    }
+      this.router.events.subscribe(event => {
+          if (event instanceof NavigationEnd) {
+              this.updateActiveItem(event.urlAfterRedirects);
+          }
+      });
 
-    logout() {
-        console.log('Logout');
-    }
+      this.loadCompanyLogo();
+  }
+
+  updateActiveItem(url: string) {
+      this.activeItem = this.items.find((item) => item.routerLink === url);
+  }
+
+  loadCompanyLogo() {
+      this.service.getImages('logo').subscribe(
+          (data: any) => {
+              if (data && data.imageUrl) {
+                  this.logoUrl = data.imageUrl; // URL da imagem recebida
+              } else {
+                  this.logoUrl = '../assets/image/default-logo.png'; // Fallback para uma imagem padrão
+              }
+          },
+          (error) => {
+              console.error('Erro ao carregar a logo:', error);
+              this.logoUrl = '../assets/image/default-logo.png'; // Caso de erro, define uma imagem padrão
+          }
+      );
+  }
+
+  logout() {
+      console.log('Logout');
+  }
 }
