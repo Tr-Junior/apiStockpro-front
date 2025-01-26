9bc6836ce66bdd7d0f00a2ddd8b7f172fe629427
Trjunior <tarcisio.junior1406@gmail.com>
2025-01-26 08:18:35 -0300

    update sales-page.component.ts

--- src/app/pages/store/sales-page/sales-page.component.ts
+++ src/app/pages/store/sales-page/sales-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { Order } from '../../../../models/order.models';
-import { ImportsService } from '../../../../services/imports.service';
-import { DataService } from '../../../../services/data.service';
+import { Order } from '../../../../../core/models/order.models';
+import { ImportsService } from '../../../../../core/services/imports.service';
+import { DataService } from '../../../../../core/services/data.service';
 import { Router } from '@angular/router';
 import { PrimeNGConfig, MessageService, ConfirmationService } from 'primeng/api';
 import { Security } from '../../../../utils/Security.util';
