2ddc450621105ce2e1b2588fcd40c6be18b10354
Trjunior <tarcisio.junior1406@gmail.com>
2025-01-26 08:16:45 -0300

    feat: create image.model.ts

---
+++ core/models/image.model.ts
@@ -0,0 +1,10 @@
+export class Image {
+  constructor(
+    public _id: string,
+    public type: string,
+    public path: number,
+    public name: string,
+    public imageUrl: string,
+    public createdAt: string
+  ) { }
+}
