c255b49da161833608806aed8b7c05065888c2af
Trjunior <tarcisio.junior1406@gmail.com>
2025-01-31 09:34:02 -0300

    refactor: Update routing to use lazy loading for components and improve 404 handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,95 +1,125 @@
 import { RouterModule, Routes } from '@angular/router';
-import { LoginPageComponent } from './pages/account/login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { FramePageComponent } from './pages/master/frame-page';
-import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 import { AuthService } from '../../core/guards/auth.service';
-import { BoxPageComponent } from './pages/store/box-page/box-page.component';
-import { SalesPageComponent } from './pages/store/sales-page/sales-page.component';
-import { BudgetPageComponent } from './pages/store/budget-page/budget-page.component';
-import { BuyPageComponent } from './pages/store/buy-page/buy-page.component';
-import { CostsPageComponent } from './pages/billing/costs-page/costs-page.component';
-import { ExitsPageComponent } from './pages/billing/exits-page/exits-page.component';
-import { NewUserPageComponent } from './pages/account/new-user-page/new-user-page.component';
-import { UsersPageComponent } from './pages/account/users-page/users-page.component';
-import { CompanyInfoPageComponent } from './company-info-page/company-info-page.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-{
-  path: '',
-  redirectTo: 'login',
-  pathMatch: 'full'
-},
-{
-  path: 'login', component: LoginPageComponent
-},
-
-{
-  path: 'store',
-  component: FramePageComponent,
-  children: [
-    { path: '', component: ProductsPageComponent, canActivate: [AuthService] },
-  ]
-},
-{
-  path: 'sale',
-  component: FramePageComponent,
-  children: [
-    { path: '', component: BoxPageComponent, canActivate: [AuthService] },
-  ]
-},
-{
-  path: 'sales',
-  component: FramePageComponent,
-  children: [
-    { path: '', component: SalesPageComponent, canActivate: [ AuthService] },
-  ]
-},
-{
-  path: '',
-  component: FramePageComponent,
-  children: [
-    { path: 'budgets', component: BudgetPageComponent, canActivate: [AuthService] },
-  ]
-},
-{
-  path: 'productsToBuy',
-  component: FramePageComponent,
-  children: [
-    { path: '', component: BuyPageComponent, canActivate: [AuthService] },
-  ]
-},
-{
-  path: 'features',
-  component: FramePageComponent,
-  children: [
-    //{ path: 'entranceAndExit', component: EntranceAndExitComponent, canActivate: [LoginRedirectGuard, AuthService] },
-   // { path: 'invoicing', component: InvoicingPageComponent, canActivate: [AuthService] },
-    { path: 'exits', component: ExitsPageComponent, canActivate: [AuthService] },
-    { path: 'details', component: CostsPageComponent, canActivate: [AuthService] }
-  ]
-},
-{
-  path: 'account',
-  component: FramePageComponent,
-  children: [
-    { path: 'new-user', component: NewUserPageComponent, canActivate: [AuthService] },
-    { path: 'passwordChange', component: UsersPageComponent, canActivate: [AuthService] },
-  ]
-},
-{
-  path: 'company',
-  component: FramePageComponent,
-  children: [
-    { path: '', component: CompanyInfoPageComponent, canActivate: [AuthService] },
-  ]
-},
-
-{
-  path: '**',
-  component: PageNotFoundComponent,
-},
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/account/login-page/login-page.component').then(m => m.LoginPageComponent)
+  },
+  {
+    path: 'store',
+    component: FramePageComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/store/products-page/products-page.component').then(m => m.ProductsPageComponent),
+        canActivate: [AuthService]
+      }
+    ]
+  },
+  {
+    path: 'sale',
+    component: FramePageComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/store/box-page/box-page.component').then(m => m.BoxPageComponent),
+        canActivate: [AuthService]
+      }
+    ]
+  },
+  {
+    path: 'sales',
+    component: FramePageComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/store/sales-page/sales-page.component').then(m => m.SalesPageComponent),
+        canActivate: [AuthService]
+      }
+    ]
+  },
+  {
+    path: '',
+    component: FramePageComponent,
+    children: [
+      {
+        path: 'budgets',
+        loadComponent: () => import('./pages/store/budget-page/budget-page.component').then(m => m.BudgetPageComponent),
+        canActivate: [AuthService]
+      }
+    ]
+  },
+  {
+    path: 'productsToBuy',
+    component: FramePageComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/store/buy-page/buy-page.component').then(m => m.BuyPageComponent),
+        canActivate: [AuthService]
+      }
+    ]
+  },
+  {
+    path: 'features',
+    component: FramePageComponent,
+    children: [
+      {
+        path: 'exits',
+        loadComponent: () => import('./pages/billing/exits-page/exits-page.component').then(m => m.ExitsPageComponent),
+        canActivate: [AuthService]
+      },
+      {
+        path: 'details',
+        loadComponent: () => import('./pages/billing/costs-page/costs-page.component').then(m => m.CostsPageComponent),
+        canActivate: [AuthService]
+      }
+    ]
+  },
+  {
+    path: 'account',
+    component: FramePageComponent,
+    children: [
+      {
+        path: 'new-user',
+        loadComponent: () => import('./pages/account/new-user-page/new-user-page.component').then(m => m.NewUserPageComponent),
+        canActivate: [AuthService]
+      },
+      {
+        path: 'passwordChange',
+        loadComponent: () => import('./pages/account/users-page/users-page.component').then(m => m.UsersPageComponent),
+        canActivate: [AuthService]
+      }
+    ]
+  },
+  {
+    path: 'company',
+    component: FramePageComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./company-info-page/company-info-page.component').then(m => m.CompanyInfoPageComponent),
+        canActivate: [AuthService]
+      }
+    ]
+  },
+  {
+    path: '404',
+    component: PageNotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
 ];
 
 @NgModule({
