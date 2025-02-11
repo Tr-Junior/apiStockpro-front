c3f5a6a836a85ec86fe8b1db4764c6844ef9de7e
Trjunior <tarcisio.junior1406@gmail.com>
2025-02-11 17:53:58 -0300

    feat: Implement user management page with user listing, editing, and deletion functionality

--- src/app/pages/account/user-management-page/user-management-page.component.ts
+++ src/app/pages/account/user-management-page/user-management-page.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { ImportsService } from '../../../../core/services/imports.service';
+import { UserService } from '../../../../core/api/user/user.service';
+import { User } from '../../../../core/models/user.model';
 
 @Component({
   selector: 'app-user-management-page',
   standalone: true,
-  imports: [],
+  imports: [ImportsService.imports],
+    providers: [ImportsService.providers],
   templateUrl: './user-management-page.component.html',
   styleUrl: './user-management-page.component.css'
 })
 export class UserManagementPageComponent {
 
+  user: User[] = [];
+constructor(
+  private userService: UserService
+){
+
+}
+
+  ngOnInit(): void {
+    this.getAllUsers();
+  }
+
+  getAllUsers(){
+    this.userService.getAllUsers().subscribe({
+      next: (data: User[]) => {
+        this.user = data;
+        console.log(this.user);
+      },
+      error: (error) => {
+        console.error('Error fetching users', error);
+      }
+    })
+  }
+
+  deleteUser(userId: string){
+    this.userService.deleteUser(userId).subscribe({
+      next: () => {
+        console.log('User deleted successfully');
+        this.getAllUsers();
+      },
+      error: (error) => {
+        console.error('Error deleting user', error);
+      }
+    })
+  }
+
+  editUser(user: any){}
 }
