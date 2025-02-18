5dc5ee74cd1a9845c4082875be785d2583430199
Trjunior <tarcisio.junior1406@gmail.com>
2025-02-18 09:46:48 -0300

    feat: Remove hardcoded Portuguese localization data from ExitsPageComponent

--- src/app/pages/billing/exits-page/exits-page.component.ts
+++ src/app/pages/billing/exits-page/exits-page.component.ts
@@ -59,18 +59,6 @@ export class ExitsPageComponent {
         Validators.required
       ])]
     });
-
-    this.ptBR = {
-      firstDayOfWeek: 0,
-      dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
-      dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
-      dayNamesMin: ["Do", "Se", "Te", "Qu", "Qu", "Se", "Sa"],
-      monthNames: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
-      monthNamesShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
-      today: 'Hoje',
-      clear: 'Limpar'
-    };
-
     this.formPaymentOptions = this.getFormPaymentExit().map(option => ({ label: option, value: option }));
 
   }
