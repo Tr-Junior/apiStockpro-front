4c21165a3e27066dde7231e0a1bf86834993cd5c
Trjunior <tarcisio.junior1406@gmail.com>
2025-02-18 09:46:40 -0300

    feat: Add Portuguese localization support in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { MessageService } from 'primeng/api';
+import { LOCALE_ID } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
     MessageService,
     ...(appConfig.providers || [])
   ]
