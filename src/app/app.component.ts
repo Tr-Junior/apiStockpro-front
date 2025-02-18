d76734efb67f6a7f131716b06e4fc09e91389fd5
Trjunior <tarcisio.junior1406@gmail.com>
2025-02-18 09:46:36 -0300

    feat: Add PrimeNG configuration for localization in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CompanyDataService } from '../core/services/company-data.service';
-import { MessageService } from 'primeng/api';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
 import { ImportsService } from '../core/services/imports.service';
 
 @Component({
@@ -15,9 +15,25 @@ import { ImportsService } from '../core/services/imports.service';
 export class AppComponent implements OnInit{
   //title = 'frontend';
 
-  constructor(private companyDataService: CompanyDataService) {}
+  constructor(
+    private companyDataService: CompanyDataService,
+    private primengConfig: PrimeNGConfig
+  ) {}
+
+
 
   async ngOnInit(): Promise<void> {
     await this.companyDataService.loadInitialData();
+
+    this.primengConfig.setTranslation({
+      firstDayOfWeek: 1,
+      dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+      dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+      dayNamesMin: ["D", "S", "T", "Q", "Q", "S", "S"],
+      monthNames: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+      monthNamesShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+      today: "Hoje",
+      clear: "Limpar"
+    });
   }
 }
